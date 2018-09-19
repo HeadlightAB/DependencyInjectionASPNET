35bfd8b5a99263708a0d901778e057e47b3ae0cc
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2018-09-19 14:54:31 +0200

    Added tests on HomeController, all green but site not running because of dependency not handled correctly, e.g. no parameterless ctor present.

--- DependencyInjectionASPNET/Controllers/HomeController.cs
+++ DependencyInjectionASPNET/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
 ﻿using System.Web.Mvc;
+using Business;
 
 namespace DependencyInjectionASPNET.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IVehicleService _service;
+
+        public HomeController(IVehicleService service)
+        {
+            _service = service;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var vehicles = _service.GetAll();
+
+            return View(vehicles);
         }
     }
 }
---
+++ Tests/HomeControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Business;
+using Core.Models;
+using DependencyInjectionASPNET.Controllers;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void ShouldReturnViewResultWhenIndex()
+        {
+            var sut = new HomeController(Substitute.For<IVehicleService>());
+
+            var result = sut.Index();
+
+            result.As<ViewResult>().Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ShouldInvokeServiceWhenIndex()
+        {
+            var service = Substitute.For<IVehicleService>();
+            var sut = new HomeController(service);
+
+            var _ = sut.Index();
+            
+            service.Received().GetAll();
+        }
+
+        [Fact]
+        public void ShouldReturnViewResultWithListWhenIndex()
+        {
+            var service = Substitute.For<IVehicleService>();
+            service.GetAll().Returns(new List<Vehicle> {new Vehicle()});
+            var sut = new HomeController(service);
+
+            var result = sut.Index().As<ViewResult>();
+
+            result.Model.As<IEnumerable<Vehicle>>().Should().NotBeEmpty();
+        }
+    }
+}
