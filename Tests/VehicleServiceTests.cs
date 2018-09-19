e90aaf436e70f7207c8f7722c4005016ded93fad
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2018-09-19 09:36:06 +0200

    Added tests and business projects with a service and an initial test, using xunit, Nsubstitute and FluentAssertions.

---
+++ Tests/VehicleServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Business;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests
+{
+    public class VehicleServiceTests
+    {
+        [Fact]
+        public void ShouldNotBeNullWhenCreated()
+        {
+            var sut = new VehicleService();
+
+            sut.Should().NotBeNull();
+        }
+    }
+}
